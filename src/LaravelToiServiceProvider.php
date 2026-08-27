4e59a879f7f3a37eeabd90218f33af72f962ef6b
Aldi Pranata <aldyjrz@gmail.com>
2026-08-27 23:54:01 +0700

    push

--- src/LaravelToiServiceProvider.php
+++ src/LaravelToiServiceProvider.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Aldytoi\LaravelToi;
 
 use Aldytoi\LaravelToi\Commands\InstallCommand;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class LaravelToiServiceProvider extends ServiceProvider
@@ -23,8 +24,28 @@ class LaravelToiServiceProvider extends ServiceProvider
             'toi'
         );
 
+        $this->registerLucideComponents();
+
         $this->commands([
             InstallCommand::class,
         ]);
+    }
+
+    private function registerLucideComponents(): void
+    {
+        $icons = [
+            'mail', 'lock', 'eye', 'eye-off', 'log-in',
+            'user', 'user-plus', 'user-check', 'user-circle',
+            'layout-dashboard', 'users', 'settings',
+            'log-out', 'search', 'bell', 'menu',
+            'chevron-down', 'check-circle',
+        ];
+
+        foreach ($icons as $icon) {
+            Blade::component(
+                'lucide-' . $icon,
+                \Aldytoi\LaravelToi\View\Components\LucideIcon::class
+            );
+        }
     }
 }
